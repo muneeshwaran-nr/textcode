5706e275e93f92c6715bc696ac036c41cd0f85ef
muneeshwaran-nr <Muneeshwaran.M@NeuralRays.AI>
2025-12-10 11:55:13 +0530

    comment out

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
-PR approve aana apram
+// PR approve aana apram
 
-Feature complete aana apram
+// Feature complete aana apram
 
-Bug fix main la add panna venumna
+// Bug fix main la add panna venumna
 
-that bug fix finally cleared:
-add panna venumna
+// that bug fix finally cleared:
+// add panna venumna
 
