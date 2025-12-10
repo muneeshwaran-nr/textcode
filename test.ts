da563b48a9f86a765504b58376967f176854cb8a
muneeshwaran-nr <Muneeshwaran.M@NeuralRays.AI>
2025-12-10 12:12:15 +0530

    fixed ts 2

--- test.ts
+++ test.ts
@@ -3,8 +3,11 @@
 // Feature complete aana apram
 
 // Bug fix main la add panna venumna
+<<<<<<< Updated upstream
 
 // that bug fix finally cleared:
 // add panna venumna
 
+=======
+>>>>>>> Stashed changes
 PR approve aana apram;
